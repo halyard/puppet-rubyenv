d5a282ab9ab8829013626832c9be906496148f5d
Les Aker <me@lesaker.org>
2015-06-09 20:19:21 -0400

    clean up module to be parameterish

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,18 @@
-class rubyenv {
+class rubyenv(
+  $versions = ['2.2.2', '2.1.6'],
+  $default = '2.2.2',
+) {
   include ruby
 
-  ruby::version { '1.8.7':
-    require     => Package['homebrew/dupes/apple-gcc42']
-  }
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.0': }
-  ruby::version { '2.1.1': }
-  ruby::version { '2.1.2': }
-  ruby::version { '2.1.3': }
-  ruby::version { '2.1.4': }
-  ruby::version { '2.1.5': }
-  ruby::version { '2.2.0': }
-  ruby::version { '2.2.1': }
-  ruby::version { '2.2.2': }
+  ruby::version { $versions: }
+
   class { 'ruby::global':
-    version => '2.2.2'
+    version => $default
+  }
+
+  ruby_gem { 'bundler for all rubies':
+    gem          => 'bundler',
+    version      => 'latest',
+    ruby_version => '*',
   }
 }
